f5414b0d4b538a7b1cdb82f92b7e730eb2b1e7a1
Scala Steward <scala_steward@virtuslab.com>
2026-09-26 18:44:35 +0000

    chore(deps): update h2 from 2.5.250 to 2.5.252

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   lazy val packageUrlJava  = "com.github.package-url"   % "packageurl-java"   % "1.5.0"
   lazy val requests        = "com.lihaoyi"             %% "requests"          % "0.9.3"
   lazy val uPickle         = "com.lihaoyi"             %% "upickle"           % "4.4.3"
-  lazy val h2              = "com.h2database"           % "h2"                % "2.5.250"
+  lazy val h2              = "com.h2database"           % "h2"                % "2.5.252"
   lazy val munit           = "org.scalameta"           %% "munit"             % "1.3.6"
   lazy val munitScalaCheck = "org.scalameta"           %% "munit-scalacheck"  % "1.3.1"
   lazy val log4jSf4jImpl   = "org.apache.logging.log4j" % "log4j-slf4j2-impl" % "2.26.1"
